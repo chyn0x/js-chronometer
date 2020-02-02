d5080e36261818bc68e826988d8137a4c6c936ff
Gabriel Ichiro <gabriel582084@gmail.com>
2020-02-02 17:55:47 -0400

    first commit: working

---
+++ script.js
@@ -0,0 +1,55 @@
+let startTime = 0, start = 0 , end = 0, diff = 0, timerId = 0 , finalTime =0;
+
+function timer(){
+	end = new Date()
+	diff = end - start
+	diff = new Date(diff)
+	let msec = diff.getMilliseconds()
+	let sec = diff.getSeconds()
+	let min = diff.getMinutes()
+	let hr = diff.getHours()-20
+	if (min < 10){
+		min = "0" + min
+	}
+	if (sec < 10){
+		sec = "0" + sec
+	}
+	if(msec < 10){
+		msec = "00" +msec
+	}
+	else if(msec < 100){
+		msec = "0" +msec
+	}
+	finalTime = document.getElementById("chronotime").innerHTML = hr + ":" + min + ":" + sec + ":" + msec
+	timerID = setTimeout("timer()", 10)
+}
+function chronoStart(){
+	document.chronoForm.startstop.value = "stop!"
+	document.chronoForm.startstop.onclick = chronoStop
+	document.chronoForm.reset.onclick = chronoReset
+	start = new Date()
+	timer()
+}
+function chronoContinue(){
+	document.chronoForm.startstop.value = "stop!"
+	document.chronoForm.startstop.onclick = chronoStop
+	document.chronoForm.reset.onclick = chronoReset
+	start = new Date()-diff
+	start = new Date(start)
+	timer()
+}
+function chronoReset(){
+	document.getElementById("chronotime").innerHTML = "0:00:00:000"
+	start = new Date()
+}
+function chronoStopReset(){
+	document.getElementById("chronotime").innerHTML = "0:00:00:000"
+	document.chronoForm.startstop.onclick = chronoStart
+}
+function chronoStop(){
+	document.chronoForm.startstop.value = "start!"
+	document.chronoForm.startstop.onclick = chronoContinue
+	document.chronoForm.reset.onclick = chronoStopReset
+	clearTimeout(timerID)
+	alert("time: " + finalTime)
+}
